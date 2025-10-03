b5558a7d89573184d865de7bbe1afb457aa08ef9
bigboom64 <sangrme@gmail.com>
2025-10-03 17:37:41 +0300

    -

--- src/microservices/events/event-service.js
+++ src/microservices/events/event-service.js
@@ -4,7 +4,7 @@ const express = require('express');
 const { Kafka } = require('kafkajs');
 
 // --- Конфигурация ---
-const KAFKA_BROKERS = ['kafka:9092']; // Замените на адрес вашего Kafka
+const KAFKA_BROKERS = ['kafka:9092']; 
 const TOPICS = {
     USER: 'users-events',
     MOVIE: 'movies-events',
